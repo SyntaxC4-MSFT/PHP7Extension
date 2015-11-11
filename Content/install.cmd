001b097edf6e5c959153747dd720c148cc474018
Cory Fowler <cory@syntaxc4.net>
2015-11-11 00:42:05 -0500

    Update install.cmd

--- Content/install.cmd
+++ Content/install.cmd
@@ -3,3 +3,10 @@ cd Commands
 
 curl -L -o PHP7x86.zip http://windows.php.net/downloads/qa/php-7.0.0RC6-nts-Win32-VC14-x86.zip
 d:\7zip\7za x PHP7x86.zip -oPHP7x86
+
+rm PHP7x86.zip
+
+## Move ApplicationHost.xdt
+
+cd ..
+cp applicationHost.xdt.bak d:\home\site\applicationHost.xdt
