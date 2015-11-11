ff4e2855c4b696910d11f3291d25a563bcc2c2b3
Cory Fowler <cory@syntaxc4.net>
2015-11-11 06:21:47 -0500

    Update install.cmd

--- Content/install.cmd
+++ Content/install.cmd
@@ -6,8 +6,6 @@ cd Commands
 curl -L -o PHP7x86.zip http://windows.php.net/downloads/qa/php-7.0.0RC6-nts-Win32-VC14-x86.zip
 d:\7zip\7za x PHP7x86.zip -oPHP7x86
 
-rm -rf PHP7x86.zip
-
 :: Create PHP.ini
 
 cd PHP7x86
@@ -19,3 +17,7 @@ sed -e 's/;fastcgi.impersonate = 1/fastcgi.impersonate = 1/g' -e 's/;fastcgi.log
 cd ..\..
 cp applicationHost.xdt d:\home\site\applicationHost.xdt
 mv applicationHost.xdt applicationHost.xdt.bak
+
+:: Clean up
+
+rm -rf PHP7x86.zip
