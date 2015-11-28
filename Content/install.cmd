b0eb3c2cb7f2b530f6963a08f6940b4490677173
Cory Fowler <cory@syntaxc4.net>
2015-11-28 04:32:10 -0800

    Update install.cmd

--- Content/install.cmd
+++ Content/install.cmd
@@ -1,20 +1,36 @@
 :: Download PHP7 x86 Runtime
 
-mkdir Commands
+IF NOT EXISTS Commands (
+  mkdir Commands
+)
+
 cd Commands
 
-curl -L -o PHP7x86.zip http://windows.php.net/downloads/qa/php-7.0.0RC7-nts-Win32-VC14-x86.zip
+IF EXISTS PHP7x86 (
+ rm -rf PHP7x86
+)
+
+curl -L -o PHP7x86.zip http://windows.php.net/downloads/qa/php-7.0.0RC8-nts-Win32-VC14-x86.zip
 d:\7zip\7za x PHP7x86.zip -oPHP7x86
 
 :: Create PHP.ini
-
 cd PHP7x86
 
 sed -e 's/;fastcgi.impersonate = 1/fastcgi.impersonate = 1/g' -e 's/;fastcgi.logging = 0/fastcgi.logging = 0/g' php.ini-production > php.ini
+echo extension_dir=ext >> php.ini
 
 :: WinCache PHP7 Binary Drop
 
+cd ext
+
+IF NOT EXISTS php_wincache.dll (
+  curl -L -O https://github.com/SyntaxC4-MSFT/PHP7Extension/releases/download/v0.7.0/php_wincache.dll
+)
+  
 :: WinCache PHP7 php.ini updates
+
+cd ..
+echo extension=php_wincache.dll >> php.ini
 
 :: Move ApplicationHost.xdt
 
