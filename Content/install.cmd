da8501724de71cb68ae120eb08650d699832a631
Cory Fowler <cfowler@microsoft.com>
2015-12-01 00:49:42 -0800

    initial commit for x64 support

--- Content/install.cmd
+++ Content/install.cmd
@@ -1,4 +1,4 @@
-:: Download PHP7 x86 Runtime
+:: Download PHP7 x64 Runtime
 
 IF NOT EXIST Commands (
   mkdir Commands
@@ -6,15 +6,15 @@ IF NOT EXIST Commands (
 
 cd Commands
 
-IF EXIST PHP7x86 (
- rm -rf PHP7x86
+IF EXIST PHP7x64 (
+ rm -rf PHP7x64
 )
 
-curl -L -o PHP7x86.zip http://windows.php.net/downloads/qa/php-7.0.0RC8-nts-Win32-VC14-x86.zip
-d:\7zip\7za x PHP7x86.zip -oPHP7x86
+curl -L -o PHP7x64.zip http://windows.php.net/downloads/qa/php-7.0.0RC8-nts-Win32-VC14-x64.zip
+d:\7zip\7za x PHP7x64.zip -oPHP7x64
 
 :: Create PHP.ini
-cd PHP7x86
+cd PHP7x64
 
 sed -e 's/;fastcgi.impersonate = 1/fastcgi.impersonate = 1/g' -e 's/;fastcgi.logging = 0/fastcgi.logging = 0/g' php.ini-production > php.ini
 echo extension_dir=ext >> php.ini
@@ -24,7 +24,7 @@ echo extension_dir=ext >> php.ini
 cd ext
 
 IF NOT EXIST php_wincache.dll (
-  curl -L -O https://github.com/SyntaxC4-MSFT/PHP7Extension/releases/download/v0.7.0/php_wincache.dll
+  curl -L -O https://github.com/SyntaxC4-MSFT/PHP7Extension/releases/download/v0.0.1/php_wincache.dll
 )
   
 :: WinCache PHP7 php.ini updates
@@ -67,4 +67,4 @@ mv applicationHost.xdt applicationHost.xdt.bak
 
 :: Clean up
 
-rm -rf PHP7x86.zip
+rm -rf PHP7x64.zip
